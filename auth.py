e35686997c4dee9104a9c714118323e69f8f94d6
Ganesh Mahajan <ganeshmahajan2004@gmail.com>
2026-09-17 20:30:26 +0530

    Updated Code

---
+++ auth.py
@@ -0,0 +1,27 @@
+from jose import jwt,JWTError
+from datetime import datetime,timedelta,timezone
+from fastapi import HTTPException,Depends
+from fastapi.security import OAuth2PasswordBearer
+
+SECRET_KEY = "mysecret"
+ALGORITH = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+oauth2_schema = OAuth2PasswordBearer(tokenUrl="Login")
+
+#token create
+def create_token(data: dict):
+    to_encode = data.copy()
+    
+    expire = datetime.NOW(timezone.utc)+ timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    
+    to_encode.update({"exp": expire})
+    
+    return jwt.encode(to_encode,SECRET_KEY,algorithem=ALGORITH)
+
+def verify_token(token: str = Depends(oauth2_schema)):
+    try:
+        payload = jwt.decode(token,SECRET_KEY,algorithms=ALGORITH)
+        return payload
+    except  JWTError:
+        raise HTTPException(status_code=401,detail="Invalid Token")
---
+++ schemas.py
@@ -0,0 +1,35 @@
+# from pydantic import BaseModel
+
+# #Input Schema
+
+# class BlogCreate(BaseModel):
+#     title:str
+#     content:str
+    
+# #Output Schema
+# class BlogResponse(BaseModel):
+#     id:int
+#     title:str
+#     content:str
+    
+#     class Config:
+#         from_attributes = True
+        
+        
+    
+    
+from pydantic import BaseModel
+
+
+class BlogCreate(BaseModel):
+    title: str
+    content: str
+
+
+class BlogResponse(BaseModel):
+    id: int
+    title: str
+    content: str
+
+    class Config:
+        from_attributes = True
